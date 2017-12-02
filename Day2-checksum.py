cf4b4e9de45ea5a4c9d4bd4e5d5dd3f2371ca099
Sumeet Bansal <sumeetbansal@gmail.com>
2017-12-02 00:43:49 -0800

    day 2 input and solutions
    a monstrosity of list comprehension

---
+++ Day2-checksum.py
@@ -0,0 +1,11 @@
+# takes input spreadsheet as tuple of tuples
+with open("Day2-input.txt", "r") as file:
+	spreadsheet = tuple([tuple(map(int, line.split('\t'))) for line in file.readlines()])
+
+# first half
+print(sum([max(row) - min(row) for row in spreadsheet]))
+
+# second half
+from itertools import combinations
+combinations = tuple([tuple(combinations(row, 2)) for row in spreadsheet])
+print(int(sum([max(pair)/min(pair) for pairs in combinations for pair in pairs if max(pair) % min(pair) == 0])))
